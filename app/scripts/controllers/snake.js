11346a7e2942aaee6f13c0a49a5c3fea88fb955c
karlos g liberal <karlosgliberal@gmail.com>
2014-03-07 12:20:21 +0100

    Control y vista para la snake TODO

---
+++ app/scripts/controllers/snake.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('matrixApp')
+  .controller('SnakeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
